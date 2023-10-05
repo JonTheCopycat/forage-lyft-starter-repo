14c1c1c6d1ffba3e7184e88abcbf860fdd0e9638
JonTheCopycat <eljonnu0906@gmail.com>
2023-10-05 19:05:17 -0400

    Learning How Python Works: import from

--- car_factory.py
+++ car_factory.py
@@ -1,26 +1,30 @@
 from car import Car
 
-from engine import CapuletEngine
-from engine import SternmanEngine
-from engine import WilloughbyEngine
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
 
-from battery import SpindlerBattery
-from battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+from battery.nubbin_battery import NubbinBattery
 
 class CarFactory:
+    @staticmethod
     def create_calliope(last_service_date, current_mileage, last_service_mileage):
         return Car( CapuletEngine(current_mileage, last_service_mileage), SpindlerBattery(last_service_date) )
     
+    @staticmethod
     def create_glissade(last_service_date, current_mileage, last_service_mileage):
         return Car( WilloughbyEngine(current_mileage, last_service_mileage), SpindlerBattery(last_service_date) )
-        
     
+    @staticmethod
     def create_palindrome(last_service_date, warning_light_on):
         return Car( SternmanEngine(warning_light_on), SpindlerBattery(last_service_date) )
     
+    @staticmethod
     def create_rorschach(last_service_date, current_mileage, last_service_mileage):
         return Car( WilloughbyEngine(current_mileage, last_service_mileage), NubbinBattery(last_service_date) )
         
     
+    @staticmethod
     def create_thovex(last_service_date, current_mileage, last_service_mileage):
         return Car( CapuletEngine(current_mileage, last_service_mileage), NubbinBattery(last_service_date) )
